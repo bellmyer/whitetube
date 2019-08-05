75929997478d72fd7f689c550cdf96c43875b812
Jaime Bellmyer <online@bellmyer.com>
2019-08-05 16:16:10 -0500

    change to hashrouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ConfigureStore } from './redux/configureStore';
 
 const store = ConfigureStore();
@@ -10,11 +10,11 @@ const store = ConfigureStore();
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <div>
           <Main />
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   );
 }
